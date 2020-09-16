f5195992c0d0d0134b8973316c7d9e4195338fcc
2018331113 <muhitmahmud404@gmail.com>
2020-09-16 21:05:51 +0600

    Updated the process logic

--- src/sample/game_logic/Process.java
+++ src/sample/game_logic/Process.java
@@ -1,6 +1,17 @@
 package sample.game_logic;
+import sample.characters.EnemyAnimation;
+import sample.characters.PlayerAnimation;
+import sample.screens.BattleScreen;
+
+
 
 public class Process {
+
+    BattleScreen screen;
+    public Process(BattleScreen screen){
+        this.screen = screen;
+    }
+
 
     double playerDamage;
     double enemyDamage;
@@ -13,12 +24,16 @@ public class Process {
         SwordAttack,MagicAttack,PowerUP,DefenseUP,Block
     }
 
-    public void playerResponse(Option option,double damage,double buff){
+    public void playerResponse(Option option,double damage,double buff) {
         playerOption = option;
         playerDamage = damage;
         playerBuff = buff;
 
-        battleProcess();
+        try {
+            battleProcess();
+        }catch (Exception exception){
+            System.out.print(exception.getMessage());
+        }
     }
 
     public void enemyResponse(Option option,double damage,double buff){
@@ -28,8 +43,66 @@ public class Process {
 
     }
 
-    void battleProcess(){
+    PlayerAnimation playerAnimation = new PlayerAnimation();
+    EnemyAnimation enemyAnimation = new EnemyAnimation();
+//    ExecutorService service = Executors.newSingleThreadExecutor();
 
+    void battleProcess() throws InterruptedException {
+        if(playerOption==Option.Block){
+            if(enemyOption==Option.Block){
+                playerAnimation.selectOption(PlayerAnimation.Options.Block);
+                enemyAnimation.selectOption(EnemyAnimation.Options.Block);
+
+                playerAnimation.block();
+                enemyAnimation.block();
+//                service.execute(playerAnimation);
+//                service.execute(enemyAnimation);
+//
+//                service.shutdown();
+
+
+//                Thread playerThread = new Thread(playerAnimation);
+//                playerThread.start();
+//                playerThread.join();
+//                playerThread.setDaemon(true);
+
+//
+//                Thread enemyThread = new Thread(enemyAnimation);
+//                enemyThread.start();
+//                enemyThread.join();
+//                enemyThread.setDaemon(true);
+
+                //playerBlock animation
+                //enemyBlock animation
+            }else if(enemyOption==Option.PowerUP){
+                //playerBlock animation
+                //enemyPowerUp animation
+            }else if(enemyOption==Option.DefenseUP){
+                //playerBlock animation
+                //enemyDefenseUp animation
+            }else if(enemyOption==Option.SwordAttack||enemyOption==Option.MagicAttack){
+                //playerBlock animation
+                //enemyMove animation
+            }
+        }else if(enemyOption==Option.Block){
+            if(playerOption==Option.PowerUP){
+                //enemyBlock animation
+                //playerPowerUp animation
+            }else if(playerOption==Option.DefenseUP){
+                //enemyBlock animation
+                //playeDefenseUp animation
+            }else if(playerOption==Option.SwordAttack||playerOption==Option.MagicAttack){
+                //enemyBlock animation
+                //playerMove animation
+            }
+        }
+
+//        choiceBox.setVisible(true);
+
+        screen.showChoices();
+
+
+        
     }
 
 }
