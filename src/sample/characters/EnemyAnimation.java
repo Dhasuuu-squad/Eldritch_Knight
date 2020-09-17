358ee2526932665b4cbd71eaf0f62408d12c1e91
2018331113 <muhitmahmud404@gmail.com>
2020-09-17 17:53:49 +0600

    added buff animation to both classes

--- src/sample/characters/EnemyAnimation.java
+++ src/sample/characters/EnemyAnimation.java
@@ -1,18 +1,36 @@
 package sample.characters;
 
+import javafx.animation.ParallelTransition;
+import javafx.animation.RotateTransition;
 import javafx.animation.TranslateTransition;
+import javafx.scene.effect.Glow;
+import javafx.scene.image.Image;
+import javafx.scene.transform.Rotate;
 import javafx.util.Duration;
 import sample.screens.BattleScreen;
 
-public class EnemyAnimation implements Runnable {
+
+public class EnemyAnimation{
+
+    BattleScreen screen;
+
     public enum Options{
         SwordAttack,MagicAttack,PowerUP,DefenseUP,Block
     }
 
     Options option;
     Enemy enemy = BattleScreen.enemy;
-    public  void selectOption(Options option){
-        this.option = option;
+    PlayerAnimation playerAnimation;
+    int turn;
+    public  void selectOption(Options option,BattleScreen screen,PlayerAnimation playerAnimation,int turn){
+            this.option = option;
+            this.screen = screen;
+            this.playerAnimation = playerAnimation;
+            this.turn = turn;
+
+            if(this.screen == null){
+                System.out.println("yes");
+            }
     }
 
     public void run(){
@@ -29,6 +47,10 @@ public class EnemyAnimation implements Runnable {
         }
     }
 
+    Glow glow = new Glow();
+    TranslateTransition transition = new TranslateTransition(Duration.seconds(1));
+    RotateTransition rotateTransition = new RotateTransition(Duration.seconds(1));
+
     void swordAttack(){
 
     }
@@ -39,13 +61,43 @@ public class EnemyAnimation implements Runnable {
 
     void powerUp(){
 
+        setBuffAnimation(enemy.powerUp);
+
+        ParallelTransition parallelTransition = new ParallelTransition(enemy.buff,transition,rotateTransition);
+
+
+        parallelTransition.play();
+        enemy.enemyObject.setEffect(glow);
+        parallelTransition.setOnFinished(actionEvent -> {
+            screen.pane.getChildren().remove(enemy.buff);
+            enemy.enemyObject.setEffect(null);
+            if(turn==0) {
+                screen.showChoices();
+            }else playerAnimation.run();
+        });
+
     }
 
     void defenseUp(){
 
+        setBuffAnimation(enemy.defenseUp);
+
+        ParallelTransition parallelTransition = new ParallelTransition(enemy.buff,transition,rotateTransition);
+
+
+        parallelTransition.play();
+        enemy.enemyObject.setEffect(glow);
+        parallelTransition.setOnFinished(actionEvent -> {
+            screen.pane.getChildren().remove(enemy.buff);
+            enemy.enemyObject.setEffect(null);
+            if(turn==0) {
+                screen.showChoices();
+            }else playerAnimation.run();
+        });
+
     }
 
-     public void block(){
+     void block(){
 
         TranslateTransition blockDefend = new TranslateTransition();
         blockDefend.setDuration(Duration.seconds(1.5));
@@ -57,8 +109,31 @@ public class EnemyAnimation implements Runnable {
         blockDefend.setOnFinished(actionEvent -> {
             enemy.enemyObject.setTranslateY(6);
             enemy.enemyObject.setImage(enemy.opponent);
+            if(turn == 0) {
+                screen.showChoices();
+            }else playerAnimation.run();
         });
 
+    }
+
+    void setBuffAnimation(Image buff){
+        enemy.buff.setImage(buff);
+        enemy.buff.setX(600);
+        enemy.buff.setY(300);
+        screen.pane.getChildren().add(enemy.buff);
+
+        glow.setLevel(.8);
+
+        transition.setNode(enemy.buff);
+        transition.setFromY(0);
+        transition.setToY(-90);
+        transition.setCycleCount(3);
+
+        rotateTransition.setNode(enemy.buff);
+        rotateTransition.setAxis(Rotate.Y_AXIS);
+        rotateTransition.setFromAngle(0);
+        rotateTransition.setByAngle(39);
+        rotateTransition.setCycleCount(3);
     }
 
 }
--- src/sample/characters/PlayerAnimation.java
+++ src/sample/characters/PlayerAnimation.java
@@ -1,11 +1,21 @@
 package sample.characters;
 
 
+import javafx.animation.ParallelTransition;
+import javafx.animation.RotateTransition;
 import javafx.animation.TranslateTransition;
+import javafx.scene.effect.Glow;
+import javafx.scene.image.Image;
+import javafx.scene.transform.Rotate;
 import javafx.util.Duration;
 import sample.screens.BattleScreen;
 
-public class PlayerAnimation implements Runnable {
+public class PlayerAnimation{
+
+    EnemyAnimation enemyAnimation;
+    public PlayerAnimation(EnemyAnimation enemyAnimation){
+        this.enemyAnimation = enemyAnimation;
+    }
 
     public enum Options{
         SwordAttack,MagicAttack,PowerUP,DefenseUP,Block
@@ -13,9 +23,13 @@ public class PlayerAnimation implements Runnable {
 
     Options option;
     Player player = BattleScreen.knight;
+    BattleScreen screen;
+    int turn;
 
-    public  void selectOption(Options option){
+    public  void selectOption(Options option,BattleScreen screen,int turn){
         this.option = option;
+        this.screen = screen;
+        this.turn = turn;
     }
 
     public void run(){
@@ -32,6 +46,10 @@ public class PlayerAnimation implements Runnable {
         }
     }
 
+    Glow glow = new Glow();
+    TranslateTransition transition = new TranslateTransition(Duration.seconds(1));
+    RotateTransition rotateTransition = new RotateTransition(Duration.seconds(1));
+
     void swordAttack(){
 
     }
@@ -42,15 +60,44 @@ public class PlayerAnimation implements Runnable {
 
     void powerUp(){
 
+        setBuffAnimation(player.powerUp);
+        ParallelTransition parallelTransition = new ParallelTransition(player.buff,transition,rotateTransition);
+
+
+        parallelTransition.play();
+        player.playerObject.setEffect(glow);
+        parallelTransition.setOnFinished(actionEvent -> {
+            screen.pane.getChildren().remove(player.buff);
+            player.playerObject.setEffect(null);
+            if(turn==0) {
+                enemyAnimation.run();
+            }else screen.showChoices();
+        });
+
+
     }
 
     void defenseUp(){
 
+        setBuffAnimation(player.defenseUp);
+        ParallelTransition parallelTransition = new ParallelTransition(player.buff,transition,rotateTransition);
+
+
+        parallelTransition.play();
+        player.playerObject.setEffect(glow);
+        parallelTransition.setOnFinished(actionEvent -> {
+            screen.pane.getChildren().remove(player.buff);
+            player.playerObject.setEffect(null);
+            if(turn==0) {
+                enemyAnimation.run();
+            }else screen.showChoices();
+        });
+
     }
 
-    public void block(){
+    void block(){
 
-
+        System.out.println("no");
         TranslateTransition blockDefend = new TranslateTransition();
         blockDefend.setDuration(Duration.seconds(1.5));
         blockDefend.setNode(player.playerObject);
@@ -61,10 +108,35 @@ public class PlayerAnimation implements Runnable {
         blockDefend.setOnFinished(actionEvent -> {
             player.playerObject.setTranslateY(6);
             player.playerObject.setImage(player.knight);
-
+            if(turn==0) {
+                enemyAnimation.run();
+            }else screen.showChoices();
         });
 
 
 
     }
+
+    void setBuffAnimation(Image buff){
+
+        player.buff.setImage(buff);
+        player.buff.setX(60);
+        player.buff.setY(300);
+        screen.pane.getChildren().add(player.buff);
+
+        glow.setLevel(.8);
+
+        transition.setNode(player.buff);
+        transition.setFromY(0);
+        transition.setToY(-90);
+        transition.setCycleCount(3);
+
+        rotateTransition.setNode(player.buff);
+        rotateTransition.setAxis(Rotate.Y_AXIS);
+        rotateTransition.setFromAngle(0);
+        rotateTransition.setByAngle(39);
+        rotateTransition.setCycleCount(3);
+
+    }
+
 }
