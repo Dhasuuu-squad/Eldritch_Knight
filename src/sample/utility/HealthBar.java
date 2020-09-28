41b8ddc2f5bd476d7e4054ad70522fdd349f582e
2018331113 <muhitmahmud404@gmail.com>
2020-09-28 23:03:06 +0600

    made a method to change the healthbars upon taking damage

--- src/sample/utility/HealthBar.java
+++ src/sample/utility/HealthBar.java
@@ -2,6 +2,7 @@ package sample.utility;
 
 import javafx.scene.Group;
 import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
@@ -12,6 +13,7 @@ public class HealthBar {
     public  String name;
     StackPane stackPane;
     Label label;
+    Rectangle healthBar;
 
     public HealthBar(double value,String label){
         health = value;
@@ -23,6 +25,7 @@ public class HealthBar {
         label.setStyle("-fx-text-fill: white;" + "-fx-font-size: 17;"+
                 "-fx-font-weight: bold;");
 
+
         Rectangle hurtBar = new Rectangle();
         hurtBar.setFill(Color.rgb(205,7,6,.87));
         hurtBar.setHeight(28);
@@ -32,12 +35,14 @@ public class HealthBar {
         hurtBar.setStroke(Color.rgb(0,0,0,.5));
         hurtBar.setStrokeWidth(10);
 
-        Rectangle healthBar = new Rectangle();
+        healthBar = new Rectangle();
         healthBar.setFill(Color.rgb(50,205,50,.87));
         healthBar.setHeight(28);
         healthBar.setWidth(health);
         healthBar.setArcWidth(30.0);
         healthBar.setArcHeight(30.0);
+
+
 
         stackPane = new StackPane(hurtBar,healthBar);
         stackPane.setLayoutY(35);
@@ -51,6 +56,21 @@ public class HealthBar {
         stackPane.setLayoutX(value1);
         label.setLayoutX(value2);
     }
+
+    double value = 0;
+    public enum Damage {
+            Player,Enemy
+    }
+    public void setDamage(double damage,Damage person){
+        value = value + damage;
+        health = health - damage;
+        healthBar.setWidth(health);
+        if(person == Damage.Player){
+            healthBar.setTranslateX(-value/2);
+        }else healthBar.setTranslateX(value/2);
+
+    }
+
 
 
 }
