a51c21d1af45434644d399dd3996819b76ba6809
2018331113 <muhitmahmud404@gmail.com>
2020-09-29 21:37:28 +0600

    ...

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -10,8 +10,6 @@ public class Main extends Application {
     public static Stage window = new Stage();
     @Override
     public void start(Stage primaryStage){
-//        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-
         window = primaryStage;
         window.setResizable(false);
         window.setOnCloseRequest(e ->{
@@ -36,8 +34,4 @@ public class Main extends Application {
     }
 }
 
-//TODO: set details of every attack :DONE
-//TODO: set info of every option :DONE
-//TODO: make health bars :DONE
-//TODO: make animations for every action
 
